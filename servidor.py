90b9632cf80a72c90065319e696d6ddb1fc2bfaf
Nieto2020 <nietoaguilar1@outlook.com>
2026-07-05 14:34:19 -0600

    fix: Mayusculas en funciones

--- servidor.py
+++ servidor.py
@@ -2,7 +2,7 @@ from http.server import *
 
 class GFG(BaseHTTPRequestHandler):
     
-    def do_get(self):
+    def do_GET(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
